ab3d032dc61b850e599cf68dae8aed647f2f860d
Carlos Bello <carlosj.hbello@gmail.com>
2024-03-09 10:26:51 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Title } from './Title';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Title />
+
+      {/* <CreateSection>
+        <CreateInput />
+        <Searcher />
+      </CreateSection>
+
+      <StartButton />
+
+      <Credits /> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Title/index.jsx
@@ -0,0 +1,12 @@
+
+
+function Title() {
+  return (
+    <div className="title-wrapper">
+        <h1>Start Learning!</h1>
+        <p>Add a new flashcard 👇</p>
+    </div>
+  )
+}
+
+export {Title};
