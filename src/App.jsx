8aac08e7a73a305045a3fd8856fb87f5be7813a7
Carlos Bello <carlosj.hbello@gmail.com>
2024-03-24 14:23:45 -0300

    practicepage working

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,76 @@ import './App.css';
 const AppContext = createContext();
 const flashcardLS = 'flashcard_list';
 
+localStorage.setItem(flashcardLS, JSON.stringify(
+  [
+    {
+      "id": 0,
+      "question": "¿Cuáles son los cinco tipos de sabores primarios?",
+      "answer": "Los sabores primarios son dulce, amargo, ácido, salado y umami.",
+      "opened": false
+    },
+    {
+      "id": 1,
+      "question": "¿Cuál es el lugar más frío de la Tierra?",
+      "answer": "La Antártida, que está cubierta de capas con un espesor de entre dos mil y tres mil metros.",
+      "opened": false
+    },
+    {
+      "id": 2,
+      "question": "¿Quién escribió La Odisea?",
+      "answer": "Este poema de 24 cantos fue escrito por Homero.",
+      "opened": false
+    },
+    {
+      "id": 3,
+      "question": "¿Cómo se llama la capital de Mongolia?",
+      "answer": "La capital de Mongolia es Ulan Bator.",
+      "opened": false
+    },
+    {
+      "id": 4,
+      "question": "¿Cuál es el río más largo del mundo?",
+      "answer": "Aunque algunos pueden creer que es el Nilo, en realidad es el Amazonas.",
+      "opened": false
+    },
+    {
+      "id": 5,
+      "question": "¿Cómo se llama la Reina del Reino Unido?",
+      "answer": "La actual monarca británica se llama Isabel II.",
+      "opened": false
+    },
+    {
+      "id": 6,
+      "question": "¿En qué continente está Ecuador?",
+      "answer": "Ecuador es un país latinoamericano y, por tanto, se encuentra en América.",
+      "opened": false
+    },
+    {
+      "id": 7,
+      "question": "¿Dónde originaron los Juegos Olímpicos?",
+      "answer": "Se originaron en Grecia, y se llamaban así porque se celebraban en la ciudad de Olimpia.",
+      "opened": false
+    },
+    {
+      "id": 8,
+      "question": "¿Qué tipo de animal es la ballena?",
+      "answer": "La ballena es un mamífero marino de hasta 30 metros de longitud.",
+      "opened": false
+    },
+    {
+      "id": 9,
+      "question": "¿De qué colores es la bandera de México?",
+      "answer": "La bandera de México está compuesta por tres colores: verde, blanco y rojo.",
+      "opened": false
+    },
+    {
+      "id": 10,
+      "question": "¿Cuántos huesos tiene el cuerpo humano?",
+      "answer": "Un adulto tiene 206 huesos, mientras que los recién nacidos tienen alrededor de 300 huesos.",
+      "opened": false
+    }
+  ]
+))
 
 function App() {
 
@@ -52,7 +122,7 @@ function App() {
 
         <FlashcardList />
 
-        <Link to='/practice'>Start Practice</Link>
+        <Link to='/practice' className="link">Start Practice</Link>
         
         <AddFlashcardBtn />
 
--- src/PracticePage/index.jsx
+++ src/PracticePage/index.jsx
@@ -1,21 +1,43 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './PracticePage.css'
 
 function PracticePage() {
 
-  const flashcards = JSON.parse(localStorage.getItem('flashcard_list'))
+  const flashcards = JSON.parse(localStorage.getItem('flashcard_list'));
+  const [counter, setCounter] = useState(1);
+  const [question, setQuestion] = useState(flashcards[0].question);
+  const [answer, setAnswer] = useState(flashcards[0].answer);
 
-  console.log(flashcards)
+  const nextFlashcard = () => {
+    setCounter(counter+1);
+    setQuestion(flashcards[counter].question);
+    setAnswer(flashcards[counter].answer);
+    console.log(counter);
+  }
 
-  return (
-    <div className="main">
-      {flashcards.map((flashcard)=>(
-        <div key={flashcard.id}>
-          <p>{flashcard.question}</p>
-          <p>{flashcard.answer}</p>
+  if (counter <= flashcards.length) {
+
+    console.log('fl', flashcards.length)
+
+    return (
+      <div className="main" onClick={nextFlashcard}>
+        <div className="question-wrapper">
+          <p className="ongame-question">{question}</p>
+          <p className="ongame-answer">{answer}</p>
         </div>
-      ))}
-    </div>
-  )
+      </div>
+    )
+
+  } else {
+    return (
+      <div className="main">
+        <h1>You finished!</h1>
+        <Link to='/' className="link">Return to Home</Link>
+      </div>
+    )
+  }
+
 }
 
 export default PracticePage;
