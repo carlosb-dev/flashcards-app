d0e747c48d000633b6ae4cd12cb29f73a290b2f3
Carlos Bello <carlosj.hbello@gmail.com>
2024-04-06 14:45:14 -0300

    url thingy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 const router = createBrowserRouter([
   {
-  path: '/',
+  path: '/flashcards-app',
   element: <App/>,
   errorElement:
   <div className='error-container'>
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
   },
 
   {
-    path: '/practice',
+    path: '/flashcards-app/practice',
     element: <PracticePage/>,
   }
 ]);
